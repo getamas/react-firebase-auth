5b0cf8e7cde6103b5bd66e97c14692e86688d8c1
Tamás Gerényi <webop.freelance@gmail.com>
2019-08-18 12:17:37 +0200

    add role based authorization

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { AuthUserContext } from '../Session';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
-import { AuthUserContext } from '../Session';
+import * as ROLES from '../../constants/roles';
 
 const Navigation = () => (
   <div>
     <AuthUserContext.Consumer>
       {authUser =>
-        authUser ? <NavigationAuth /> : <NavigationNonAuth />
+        authUser ? (
+            <NavigationAuth authUser={authUser}/>
+          ) : (
+            <NavigationNonAuth />
+          )
       }
     </AuthUserContext.Consumer>
   </div>
 );
 
-const NavigationAuth = () => (
+const NavigationAuth = ({ authUser }) => (
   <ul>
     <li>
       <Link to={ROUTES.LANDING}>Landing</Link>
@@ -26,9 +31,11 @@ const NavigationAuth = () => (
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
+    {!!authUser.roles[ROLES.ADMIN] && (
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    )}
     <li>
       <SignOutButton />
     </li>
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -4,6 +4,7 @@ import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
 
 const SignUpPage = () => {
   return (
@@ -19,6 +20,7 @@ const INITIAL_STATE = {
   email: '',
   passwordOne: '',
   passwordTwo: '',
+  isAdmin: false,
   error: null
 };
 
@@ -30,7 +32,13 @@ class SignUpFormBase extends Component {
   }
 
   onSubmit = event => {
-    const { username, email, passwordOne } = this.state;
+    const { username, email, passwordOne, isAdmin } = this.state;
+
+    const roles = {};
+
+    if (isAdmin) {
+      roles[ROLES.ADMIN] = ROLES.ADMIN;
+    }
 
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
@@ -41,6 +49,7 @@ class SignUpFormBase extends Component {
           .set({
             username,
             email,
+            roles
           });
       })
       .then(() => {
@@ -58,12 +67,17 @@ class SignUpFormBase extends Component {
     this.setState({ [event.target.name]: event.target.value })
   }
 
+  onChangeCheckbox = event => {
+    this.setState({ [event.target.name]: event.target.checked });
+  };
+
   render() {
     const {
       username,
       email,
       passwordOne,
       passwordTwo,
+      isAdmin,
       error
      } = this.state;
 
@@ -103,7 +117,15 @@ class SignUpFormBase extends Component {
           type="password"
           placeholder="Confirm Password"
         />
-
+        <label>
+          Admin:
+          <input
+            name="isAdmin"
+            type="checkbox"
+            checked={isAdmin}
+            onChange={this.onChangeCheckbox}
+          />
+        </label>
         <button disabled={isInvalid} type="submit">Sign Up</button>
 
         {error && <p>{error.message}</p>}
