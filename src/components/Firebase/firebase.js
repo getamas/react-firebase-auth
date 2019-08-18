6f556214c3f3c96d6f4858c19ce49c629ef610ce
Tamás Gerényi <webop.freelance@gmail.com>
2019-08-18 13:39:17 +0200

    add google social login

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,6 +17,8 @@ class Firebase {
 
     this.auth = app.auth();
     this.db = app.database();
+
+    this.googleProvider = new app.auth.GoogleAuthProvider();
   }
 
   // *** Auth API ***
@@ -26,6 +28,9 @@ class Firebase {
 
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignInWithGoogle = () =>
+    this.auth.signInWithPopup(this.googleProvider);
 
   doSignOut = () => this.auth.signOut();
 
